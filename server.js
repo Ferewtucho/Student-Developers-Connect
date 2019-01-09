6c2c43f16d6679f53ff2b271ff192f205e07ecfd
bilu k <contactnebey@yahoo.com>
2019-01-09 18:10:56 -0500

    initial express server with route files

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const mongoose = require("mongoose");
 const app = express();
 
 //DB Config
+const users = require("./routes/api/users");
+const post = require("./routes/api/post");
+const profile = require("./routes/api/profile");
 
 const db = require("./config/keys").mongoURI;
 
@@ -18,5 +21,8 @@ mongoose
 app.get("/", (req, res) => res.send("Hello World!"));
 
 const port = process.env.PORT || 5000;
-
+//Use Routes
+app.use("/api/users", users);
+app.use("/api/post", post);
+app.use("/api/profile", profile);
 app.listen(port, () => console.log(`Server running on port ${port}`));
