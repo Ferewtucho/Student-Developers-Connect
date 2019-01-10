03610ebff7abd6c6fce2e4f5719e9b0c024703cd
Ferew Tucho <ferewtucho@gmail.com>
2019-01-10 12:32:08 -0500

    passport jwt authentication strategy test

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const passport = require("passport");
 
 const keys = require("../../config/keys");
 
@@ -95,5 +96,23 @@ router.post("/login", (req, res) => {
     });
   });
 });
+
+// @route  GET api/users/current
+// @des    Return current user
+// @access Private
+
+router.get(
+  "/current",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    // res.json({ msg: "Success" });
+    // res.json(req.user);
+    res.json({
+      id: req.user.id,
+      name: req.user.name,
+      email: req.user.email
+    });
+  }
+);
 
 module.exports = router;
