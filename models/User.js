c3a04bfcb7c7c4cbb2ff6afa210a06287a6e1496
Ferew Tucho <ferewtucho@gmail.com>
2019-01-10 20:37:08 -0500

    fix some sapce to make it clean

--- models/User.js
+++ models/User.js
@@ -8,17 +8,21 @@ const UserSchema = new Schema({
     type: String,
     required: true
   },
+
   email: {
     type: String,
     required: true
   },
+
   password: {
     type: String,
     required: true
   },
+
   avatar: {
     type: String
   },
+
   date: {
     type: Date,
     default: Date.now
