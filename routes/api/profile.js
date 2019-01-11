a0f82a2ae0feb32004abba902df425e7d3aa1f3d
bilu k <contactnebey@yahoo.com>
2019-01-10 20:32:12 -0500

    update profile to create and update profile routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,12 +1,44 @@
 //fetch creat proofiles//location//bio//expreiances .....
 //anything that has to do with autonication will be here
 const express = require("express");
+
 const router = express.Router();
+
+const mongoose = require("mongoose");
+
+const passport = require("passport");
+
+// Load Profile Model
+const Profile = require("../../models/Profile");
+
+//Load User Model
+const User = require("../../models/User");
 
 // @route  GET api/profile/test
 // @des    Test Profile routes
 // @access Public rote
-
 router.get("/test", (req, res) => res.json({ msg: "Profile Works" }));
+
+// @route  GET api/profile
+// @des    Get current users profile
+// @access Private
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const errors = {};
+
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        if (!profile) {
+          errors.nonprofile = "There is no profile for this user";
+          return res.status(404).json(errors);
+        }
+
+        res.json(profile);
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
 
 module.exports = router;
