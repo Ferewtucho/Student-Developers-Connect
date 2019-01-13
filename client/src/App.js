917c7301087711648df4cf304707ecb07841a6a6
Ferew Tucho <ferewtucho@gmail.com>
2019-01-13 11:00:18 -0500

    Basic Layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,20 @@
 import React, { Component } from "react";
+
+import Navbar from "./components/layout/Navbar";
+
+import Landing from "./components/layout/Landing";
+
+import Footer from "./components/layout/Footer";
+
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>My React App</h1>
+        <Navbar />
+        <Landing />
+        <Footer />
       </div>
     );
   }
