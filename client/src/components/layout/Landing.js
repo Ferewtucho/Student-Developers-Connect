659ce2dab10b431614740d5598208d6f78643910
Ferew  Tucho <38511905+Ferewtucho@users.noreply.github.com>
2019-01-14 10:36:48 -0500

    updated React Router & Components State Error handling && Dipslay updated (#23)

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class Landing extends Component {
-  state = {};
   render() {
     return (
       <div className="landing">
@@ -9,19 +9,19 @@ class Landing extends Component {
           <div className="container">
             <div className="row">
               <div className="col-md-12 text-center">
-                <h1 className="display-3 mb-4">Student Developers Connector</h1>
+                <h1 className="display-3 mb-4">Developer Connector</h1>
                 <p className="lead">
                   {" "}
                   Create a developer profile/portfolio, share posts and get help
                   from other developers
                 </p>
                 <hr />
-                <a href="register.html" className="btn btn-lg btn-info mr-2">
+                <Link to="/register" className="btn btn-lg btn-info mr-2">
                   Sign Up
-                </a>
-                <a href="login.html" className="btn btn-lg btn-light">
+                </Link>
+                <Link to="/login" className="btn btn-lg btn-light">
                   Login
-                </a>
+                </Link>
               </div>
             </div>
           </div>
