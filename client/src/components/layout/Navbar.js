72ef074b81f12202464d4e5c99726abda4ab5d80
Ferew Tucho <ferewtucho@gmail.com>
2019-01-16 10:54:05 -0500

    updated profile actions comonent , delet account, edit profile comonent , add expericance form

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -18,6 +18,11 @@ class Navbar extends Component {
     const authLinks = (
       <ul className="navbar-nav ml-auto">
         <li className="nav-item">
+          <Link className="nav-link" to="/dashboard">
+            Dashboard
+          </Link>
+        </li>
+        <li className="nav-item">
           <a href=" " onClick={this.onLogoutClick} className="nav-link">
             <img
               className="rounded-circle"
