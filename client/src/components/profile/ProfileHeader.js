4019b1a306271c146f6c2d812d1fe6b95377eb13
bilu k <contactnebey@yahoo.com>
2019-01-16 19:38:13 -0500

    update profile by handle and sub components

---
+++ client/src/components/profile/ProfileHeader.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class ProfileHeader extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <h1>TODO: PROFILE Header</h1>
+      </div>
+    );
+  }
+}
+
+export default ProfileHeader;
