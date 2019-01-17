761b50d0474972653280772447abec064a76a413
nebiyouk <contactnebey@yahoo.com>
2019-01-16 19:39:57 -0500

    update profile by handle and sub components (#36)
    * updated profile component and get profiles action
    
    * update profile by handle and sub components

---
+++ client/src/components/profile/ProfileAbout.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class ProfileAbout extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <h1>TODO: PROFILE ABOUT</h1>
+      </div>
+    );
+  }
+}
+
+export default ProfileAbout;
---
+++ client/src/components/profile/ProfileGithub.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class ProfileGithub extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <h1>TODO: PROFILE GITHUB</h1>
+      </div>
+    );
+  }
+}
+
+export default ProfileGithub;
