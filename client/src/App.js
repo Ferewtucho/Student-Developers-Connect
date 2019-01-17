0c81f275c0322d9a24ec480f1e40a0ab831f33bb
bilu k <contactnebey@yahoo.com>
2019-01-17 11:37:04 -0500

    updated get posts action and post feed components

--- client/src/App.js
+++ client/src/App.js
@@ -22,10 +22,11 @@ import AddExperience from "./components/add-credentials/AddExperience";
 import AddEducation from "./components/add-credentials/AddEducation";
 import Profiles from "./components/profiles/Profiles";
 import Profile from "./components/profile/Profile";
+import Posts from "./components/posts/Posts";
+import NotFound from "./components/not-found/NotFound";
 
 import "./App.css";
 import { decode } from "punycode";
-import NotFound from "./components/not-found/NotFound";
 
 // Check for token
 if (localStorage.jwtToken) {
@@ -102,6 +103,11 @@ class App extends Component {
                   component={AddEducation}
                 />
               </Switch>
+
+              <Switch>
+                <PrivateRoute exact path="/feed" component={Posts} />
+              </Switch>
+
               <Route exact path="/not-found" component={NotFound} />
             </div>
             <Footer />
