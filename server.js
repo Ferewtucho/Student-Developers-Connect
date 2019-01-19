76965ba8f973c64e82d64d95bf2eee0e1ea7ad3f
Ferew  Tucho <38511905+Ferewtucho@users.noreply.github.com>
2019-01-19 11:37:36 -0500

    deploye (#50)
    * prepared for deploy
    
    * prepared for deploy
    
    * change
    
    * delply

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
 const posts = require("./routes/api/posts");
 
+const app = express();
+
 // Server static assets if in production
 if (process.env.NODE_ENV === "production") {
   // Set static folder
@@ -17,8 +19,6 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
-
-const app = express();
 
 // Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
