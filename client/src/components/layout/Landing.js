cb2439df73e8b983262bed9b467bb4be41370597
bilu k <contactnebey@yahoo.com>
2019-01-21 11:48:49 -0500

    updated nav bar content

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -17,7 +17,7 @@ class Landing extends Component {
           <div className="container">
             <div className="row">
               <div className="col-md-12 text-center">
-                <h1 className="display-3 mb-4">Student Developer Connector</h1>
+                <h1 className="display-3 mb-4">Software Developer Connector</h1>
                 <p className="lead">
                   {" "}
                   Create a developer profile/portfolio, share posts and get help
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -61,7 +61,7 @@ class Navbar extends Component {
       <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
         <div className="container">
           <Link className="navbar-brand" to="/">
-            StuDevConnector
+            SoftDevConnector
           </Link>
           <button
             className="navbar-toggler"
